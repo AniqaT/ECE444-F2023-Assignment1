0c5057fc5b2e57b43f3799cdfddb767e13449144
AniqaT <aniqatahseen1@gmail.com>
2023-09-11 19:36:43 -0400

    Added one more test case for the reversed function in utils_test.py

--- utils_tests.py
+++ utils_tests.py
@@ -7,6 +7,7 @@ class utils_tests ():
         assert utils.reversed("75") == "Input must be an int", "Should be \"Input must be an int\""
         assert utils.reversed(87) == 78, "Should be 78"
         assert utils.reversed(79) == 97, "Should be 97"
+        assert utils.reversed(87) == 78, "Should be 78"
 
     def test_formatter():
         assert utils.formatter(95) == ['0b1011111', '0o137'], "Should be ['0b1011111', '0o137']"
