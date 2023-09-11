3c12192b0d7656540e2beb104a887a93d49e3f29
AniqaT <aniqatahseen1@gmail.com>
2023-09-11 19:11:59 -0400

    Developed test cases in the utils_tests.py file for the functions in the utils class

---
+++ utils_tests.py
@@ -0,0 +1,17 @@
+from utils import utils
+
+class utils_tests ():
+    def test_reversed():
+        assert utils.reversed(95) == 59, "Should be 59"
+        assert utils.reversed(59.5) == "Input must be an int", "Should be \"Input must be an int\""
+        assert utils.reversed("75") == "Input must be an int", "Should be \"Input must be an int\""
+
+    def test_formatter():
+        assert utils.formatter(95) == ['0b1011111', '0o137'], "Should be ['0b1011111', '0o137']"
+        assert utils.formatter(59.5) == "Input must be an int", "Should be \"Input must be an int\""
+        assert utils.formatter("75") == "Input must be an int", "Should be \"Input must be an int\""
+
+if __name__ == "__main__":
+    utils_tests.test_reversed()
+    utils_tests.test_formatter()
+    print("All tests passed")
